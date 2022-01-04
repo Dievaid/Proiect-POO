5fb65ea08809f79c464e985ba7989b103758fab0
Dievaid <davidcapragiu.esports@gmail.com>
2022-01-05 00:45:22 +0200

    Input loading into database (used as singleton)

---
+++ src/exec/Database.java
@@ -0,0 +1,52 @@
+package exec;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import models.AnnualChildren;
+import input.loader.Input;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public final class Database {
+    @JsonIgnore
+    private static Database instance = new Database();
+
+    @JsonProperty("annualChildren")
+    private List<AnnualChildren> annualChildren = new ArrayList<>();
+
+    private Database() { }
+
+    /**
+     * Method used for getting the current instance of the singleton
+     * @return Current instance of the database
+     */
+    public static Database getInstance() {
+        if (instance == null) {
+            return new Database();
+        }
+        return instance;
+    }
+
+    /**
+     * Method used for simulating each year
+     * @param data -> parsed JSON data from a specific test file
+     */
+    public void fetchData(final Input data) {
+        data.simulateFirstYear();
+        data.simulateAnnualChanges();
+    }
+
+    /**
+     * Method used for deleting the current state of the singleton
+     * Deleting the current state of the singleton, prepares the simulation for another round
+     * Used for avoiding data parsed from previous inputs
+     */
+    public static void clear() {
+        instance = new Database();
+    }
+}
---
+++ src/exec/Simulator.java
@@ -0,0 +1,44 @@
+package exec;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import common.Constants;
+import input.loader.Input;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+@Getter
+@Setter
+public class Simulator {
+    private Input input;
+
+    public Simulator(final String inputFileName) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        input = mapper.readValue(Paths.get(inputFileName).toFile(), Input.class);
+    }
+
+    /**
+     * Method used for parsing the input and processing it
+     * Used in the main class as the entry point
+     * @throws IOException in case the classes are not organised for parsing json into Java objects
+     */
+    public static void execute() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        for (int i = 1; i <= Constants.TESTS_NUMBER; i++) {
+            String inputFile = String.format("./tests/test%d.json", i);
+            String outputFile = String.format("./output/out_%d.json", i);
+            Simulator simulator = new Simulator(inputFile);
+            Database.getInstance().fetchData(simulator.getInput());
+            String outputFileContent = mapper.writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(Database.getInstance());
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+            writer.write(outputFileContent);
+            writer.close();
+            Database.clear();
+        }
+    }
+}
