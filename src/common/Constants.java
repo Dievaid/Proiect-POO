82dfedf8a8bffeb48a1fe068512a6621290eb895
Dievaid <davidcapragiu.esports@gmail.com>
2022-01-23 20:02:16 +0200

    New useful constants

--- src/common/Constants.java
+++ src/common/Constants.java
@@ -6,7 +6,7 @@ public final class Constants {
     public static final String FILE_EXTENSION = ".json";
     public static final Integer SMALL_TEST_POINTS = 2;
     public static final Integer BIG_TEST_POINTS = 3;
-    public static final Integer TESTS_NUMBER = 25;
+    public static final Integer TESTS_NUMBER = 30;
     public static final Integer MAXIMUM_ERROR_CHECKSTYLE = 30;
     public static final Integer CHECKSTYLE_POINTS = 10;
     public static final Integer TESTS_NUMBER_SMALL = 15;
@@ -18,5 +18,11 @@ public final class Constants {
     public static final int BABY = 5;
     public static final int KID = 12;
     public static final int TEEN = 18;
+    public static final Integer SCALE = 30;
+    public static final Integer ONE_HUNDRED = 100;
     public static final Double MAX_SCORE = 10.0;
+    public static final String YELLOW = "yellow";
+    public static final String ID = "id";
+    public static final String NICE_SCORE = "niceScore";
+    public static final String NICE_SCORE_CITY = "niceScoreCity";
 }
